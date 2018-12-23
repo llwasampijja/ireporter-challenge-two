6043d43d513c840f0ced66195649b6ef64c1ceed
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-12-23 13:41:04 +0300

    Re-optmised the code redflag_controller.py

--- app/controllers/redflag_controller.py
+++ app/controllers/redflag_controller.py
@@ -26,21 +26,21 @@ class RedflagController:
 
 
         get_redflags_instance = redflag_data.get_redflags()
-        if len(get_redflags_instance) <= 0:
+        if not get_redflags_instance:
             redflag_id = 1
         else:
             redflag_id = 1 + get_redflags_instance[-1].get("redflag_id")
 
         if any(self.redflag_validator.check_empty_string(item) for item in
                args_strings):
-            return self.response_emptystring()
+            return self.responses("empty", None)
 
         if any(self.redflag_validator.check_str_datatype(item) for item in
                args_strings):
-            return self.response_unaccepted("datatype")
+            return self.responses("datatype", None)
 
         if self.redflag_validator.check_status_value(status):
-            return self.response_unaccepted("status")
+            return self.responses("status", None)
 
         new_redflag = RedFlag(redflag_id=redflag_id, report_type=report_type,
                               created_on=created_on, created_by=created_by,
@@ -48,73 +48,75 @@ class RedflagController:
                               images=images, comment=comment)
         redflag_data.create_redflag(new_redflag.redflag_dict())
 
-        response_data = {
-            "status": 202,
-            "data": [new_redflag.redflag_dict()],
-            "message": "Red-Flag created successifully"
-        }
-        return Response(json.dumps(response_data),
-                        content_type="application/json", status=202)
+        return self.responses("create_success", new_redflag.redflag_dict())
 
     def get_reflags(self):
         get_redflags_instance = redflag_data.get_redflags()
-        if len(get_redflags_instance) <= 0:
-            return Response(json.dumps({
-                "status": 411,
-                "message": "Redflags list is empty"
-            }), content_type="application/json", status=411)
+        if not get_redflags_instance:
+            return self.responses("empty_list", None)
+            # return Response(json.dumps({
+            #     "status": 411,
+            #     "message": "Redflags list is empty"
+            # }), content_type="application/json", status=411)
         else:
-            return Response(json.dumps({
-                "status": 200,
-                "data": get_redflags_instance
-            }), content_type="application/json", status=200)
+            return self.responses("non_empty_list", get_redflags_instance)
+            # return Response(json.dumps({
+            #     "status": 200,
+            #     "data": get_redflags_instance
+            # }), content_type="application/json", status=200)
 
     def get_redflag(self, redflag_id):
         get_redflag_instance = redflag_data.get_redflag(redflag_id)
         if get_redflag_instance is None:
-            return Response(json.dumps({
-                "status": 404,
-                "message": "No redflag of that specific id found"
-            }), content_type="application/json", status=404)
+            return self.responses("none", None)
         else:
-            return Response(json.dumps({
-                "status": 200,
-                "data": [get_redflag_instance]
-            }), content_type="application/json", status=200)
+            return self.responses("non_empty_list", get_redflag_instance)
+
+        # if get_redflag_instance is None:
+        #     return Response(json.dumps({
+        #         "status": 404,
+        #         "message": "No redflag of that specific id found"
+        #     }), content_type="application/json", status=404)
+        # else:
+        #     return Response(json.dumps({
+        #         "status": 200,
+        #         "data": [get_redflag_instance]
+        #     }), content_type="application/json", status=200)
 
     def update_redflag(self, redflag_id, request_data):
 
         comment = request_data.get("comment")
 
         if self.redflag_validator.check_empty_string(comment):
-            return self.response_emptystring()
+            # return self.response_emptystring()
+            return self.responses("empty", None)
 
         if self.redflag_validator.check_str_datatype(comment):
-            return self.response_unaccepted("datatype")
+            return self.responses("datatype", None)
 
         update_redflag_instance = redflag_data.update_redflag(
             redflag_id, request_data)
         if update_redflag_instance is None:
-            return self.response_unaccepted("none")
+            return self.responses("none", None)
         else:
-            return self.response_sumission_success(update_redflag_instance,
-                                                   "update")
+            # return self.response_sumission_success(update_redflag_instance, "update")
+            return self.responses("update_success", update_redflag_instance)
 
     def delete_redflag(self, redflag_id):
         delete_redflag_instance = redflag_data.delete_redflag(redflag_id)
         if delete_redflag_instance is None:
-            return self.response_unaccepted("none")
+            return self.responses("none", None)
         else:
-            return self.response_sumission_success(delete_redflag_instance,
-                                                   "delete")
+            return self.responses("delete_success", delete_redflag_instance)
+            # return self.response_sumission_success(delete_redflag_instance, "delete")
 
-    def response_emptystring(self):
-        return Response(json.dumps({
-            "status": 406,
-            "message": "No empty fields are allowed"
-        }), content_type="application/json", status=406)
+    # def response_emptystring(self):
+    #     return Response(json.dumps({
+    #         "status": 406,
+    #         "message": "No empty fields are allowed"
+    #     }), content_type="application/json", status=406)
 
-    def response_unaccepted(self, word):
+    def responses(self, word, data):
         if word == "none":
             status_code = 404
             message = "No redflag of that specific id found"
@@ -124,21 +126,48 @@ class RedflagController:
         elif word == "empty":
             status_code = 406
             message = "No empty fields are allowed"
+        elif word == "create_success":
+            status_code = 202
+            message = "Red-Flag created successifully"
+        elif word == "update_success":
+            status_code = 202
+            message = "Red-flag updated successfully" 
+        elif word == "delete_success":
+            status_code = 202
+            message = "Red-flag deleted successfully"  
+        elif word == "empty_list":
+            status_code = 411
+            message = "Redflags list is empty"
+        elif word == "non_empty_list":
+            status_code = 200
+            message = None
         else:
             status_code = 406
             message = "Unaccepted datatype"
-        return Response(json.dumps({
-            "status": status_code,
-            "message": message
-        }), content_type="application/json", status=status_code)
-
-    def response_sumission_success(self, return_data, keyword):
-        if keyword == "delete":
-            message = "Red-flag deleted successfully"
+        if data is None:
+            return Response(json.dumps({
+                "status": status_code,
+                "message": message
+            }), content_type="application/json", status=status_code)
+        elif message is None:
+            return Response(json.dumps({
+                "status": status_code,
+                "data": [data],
+            }), content_type="application/json", status=status_code)
         else:
-            message = "Updated red-flag record’s location"
-        return Response(json.dumps({
-            "status": 202,
-            "data": [return_data],
-            "message": message
-        }), content_type="application/json", status=202)
+            return Response(json.dumps({
+                "status": status_code,
+                "data": [data],
+                "message": message
+            }), content_type="application/json", status=status_code)
+
+    # def response_sumission_success(self, return_data, keyword):
+    #     if keyword == "delete":
+    #         message = "Red-flag deleted successfully"
+    #     else:
+    #         message = "Updated red-flag record’s location"
+    #     return Response(json.dumps({
+    #         "status": 202,
+    #         "data": [return_data],
+    #         "message": message
+    #     }), content_type="application/json", status=202)
