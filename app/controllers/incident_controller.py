12a93832292f53c1b329eee358c2425864927aac
Lamech Lwasampijja <llwasampijja@gmail.com>
2019-01-06 17:35:38 +0300

    Refacted code for delete and updating controller.py

--- app/controllers/incident_controller.py
+++ app/controllers/incident_controller.py
@@ -91,6 +91,30 @@ class IncidentController:
                 "data": [get_incident_instance]
             }), content_type="application/json", status=200)
 
+    # def update_incident(self, incident_id, request_data, keyword, username):
+
+    #     location = request_data.get("location")
+
+    #     if self.validator.check_empty_string(location):
+    #         return self.response_unaccepted("empty")
+
+    #     if self.validator.check_str_datatype(location):
+    #         return self.response_unaccepted("datatype")
+
+    #     update_incident_instance = incident_data.update_incident(
+    #         incident_id, request_data, keyword, username)
+    #     if update_incident_instance is None:
+    #         return self.response_unaccepted("none")
+    #     elif update_incident_instance == "non_author":
+    #         return Response(json.dumps({
+    #             "status": 401,
+    #             "message": RESP_UNAUTHORIZED_EDIT
+    #         }), content_type="application/json", status=401)
+    #     else:
+    #         return self.response_sumission_success(update_incident_instance,
+    #                                                "update")
+
+
     def update_incident(self, incident_id, request_data, keyword, username):
 
         location = request_data.get("location")
@@ -103,16 +127,8 @@ class IncidentController:
 
         update_incident_instance = incident_data.update_incident(
             incident_id, request_data, keyword, username)
-        if update_incident_instance is None:
-            return self.response_unaccepted("none")
-        elif update_incident_instance == "non_author":
-            return Response(json.dumps({
-                "status": 401,
-                "message": RESP_UNAUTHORIZED_EDIT
-            }), content_type="application/json", status=401)
-        else:
-            return self.response_sumission_success(update_incident_instance,
-                                                   "update")
+        return self.delete_update(update_incident_instance, RESP_UNAUTHORIZED_EDIT, self.response_sumission_success(update_incident_instance,
+                                                   "update"))
 
     def update_incident_status(self, incident_id, request_data, keyword):
         # if "status" not in request_data or len(request_data) != 1 or self.validator.check_status_value(request_data.get("status")):
@@ -133,19 +149,36 @@ class IncidentController:
             return self.response_sumission_success(update_incident_instance,
                                                    "incident_status")
 
+    # def delete_incident(self, incident_id, keyword, username):
+    #     delete_incident_instance = incident_data.delete_incident(
+    #         incident_id, keyword, username)
+    #     if delete_incident_instance is None:
+    #         return self.response_unaccepted("none")
+    #     elif delete_incident_instance == "non_author":
+    #         return Response(json.dumps({
+    #             "status": 401,
+    #             "message": RESP_UNAUTHORIZED_DELETE
+    #         }), content_type="application/json", status=401)
+    #     else:
+    #         return self.response_sumission_success(delete_incident_instance,
+    #                                                "delete")
+
     def delete_incident(self, incident_id, keyword, username):
         delete_incident_instance = incident_data.delete_incident(
             incident_id, keyword, username)
-        if delete_incident_instance is None:
+        return self.delete_update(delete_incident_instance, RESP_UNAUTHORIZED_DELETE, self.response_sumission_success(delete_incident_instance,
+                                                   "delete"))
+
+    def delete_update(self, action_instance, message_fail, message_success):
+        if action_instance is None:
             return self.response_unaccepted("none")
-        elif delete_incident_instance == "non_author":
+        elif action_instance == "non_author":
             return Response(json.dumps({
                 "status": 401,
-                "message": RESP_UNAUTHORIZED_DELETE
+                "message": message_fail
             }), content_type="application/json", status=401)
         else:
-            return self.response_sumission_success(delete_incident_instance,
-                                                   "delete")
+            return message_success
 
     def response_unaccepted(self, word):
         if word == "none":
